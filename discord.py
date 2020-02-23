06e99632c5926eca7402e015b7c3edc40344e019
wys7600 <61371738+wys7600@users.noreply.github.com>
2020-02-23 15:06:04 +0900

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 
 client = discord.Client()
@@ -16,7 +17,8 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith("안녕"):
         await message.channel.send("할말")
-
-
-client.run("NjgwMDM4NTMxMjIxMDI4ODc2.XlIJ9Q.LaA6jNKHG04HG8COHNf5hm9AeTY")
+        
+        
+access_token = os.environ["BOT_TOKEN"]
+client.run(acess_token)
                                 
